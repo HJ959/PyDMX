389292f95680b9bf4f257992aaa85fed8ef12eb0
HJ959 <henryjaames@gmail.com>
2021-03-26 16:28:46 +0000

    Added RMS function to make the DMX lighting sound responsive, needs filtering for better effect

--- example.py
+++ example.py
@@ -1,10 +1,45 @@
 from dmx import Colour, DMXInterface, DMXLight3Slot, DMXLightUking, DMXUniverse
 from time import sleep
 from sys import exit as sys_exit
+import pyaudio
+import time
+import audioop
 
 RED = Colour(255,0,0)
 GREEN = Colour(0,255,0)
 BLUE = Colour(0,0,255)
+###########################################################################
+def get_rms():
+    # Creates a generator that can iterate rms values
+    CHUNK = 1024
+    WIDTH = 2
+    CHANNELS = 1
+    RATE = 44100
+    colourMax = 255
+
+    p = pyaudio.PyAudio()
+
+    try:
+        stream = p.open(format=p.get_format_from_width(WIDTH),
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        output=True,
+                        frames_per_buffer=CHUNK)
+
+        time.sleep(1)
+        while True:
+            data = stream.read(CHUNK, exception_on_overflow = False)
+            rms = audioop.rms(data, 2)
+            # Scale the rms value to be within 0-255
+            rms_scaled = (rms / 1024) * 255
+            if rms_scaled <= colourMax and rms_scaled >= 0:
+                yield rms_scaled
+    finally:
+        p.terminate()
+        stream.stop_stream()
+        stream.close()
+
 ##############################################################################
 def set_and_update(universe, interface):
     # update the interface's frame to be the universe's current state
@@ -14,6 +49,9 @@ def set_and_update(universe, interface):
 
 ##############################################################################
 def main():
+    # create an instance of the RMS generator
+    audio_feed = get_rms()
+
     # Open an interface
     with DMXInterface("FT232R") as interface:
         # create a universe
@@ -37,14 +75,14 @@ def main():
         seq_two = [GREEN, BLUE, RED, BLUE, BLUE]
 
 
-        for _ in range(0,50):  
-            light.set_brightness(255)
+        for rms in audio_feed:  
+            light.set_brightness(rms)
             light.set_colour(GREEN)
             light_two.set_colour(BLUE)
-            light_two.set_brightness(255)
+            light_two.set_brightness(rms)
             # set frame and update
             set_and_update(universe, interface)
-            sleep(0.5 - (15.0 / 1000.0))
+            #sleep(0.5 - (15.0 / 1000.0))
         
         light.set_brightness(0)
         light_two.set_brightness(0)
