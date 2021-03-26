9e713cb1ef8ce8bf26ca539021013530a50982db
HJ959 <henryjaames@gmail.com>
2021-03-26 17:53:49 +0000

    Trying to implement a simple low pass filter

--- example.py
+++ example.py
@@ -4,36 +4,62 @@ from sys import exit as sys_exit
 import pyaudio
 import time
 import audioop
+import numpy as np
 
 RED = Colour(255,0,0)
 GREEN = Colour(0,255,0)
 BLUE = Colour(0,0,255)
+
+
+
+fc = 0.1  # Cutoff frequency as a fraction of the sampling rate (in (0, 0.5)).
+b = 0.08  # Transition band, as a fraction of the sampling rate (in (0, 0.5)).
+N = int(np.ceil((4 / b)))
+if not N % 2: N += 1  # Make sure that N is odd.
+n = np.arange(N)
+ 
+# Compute sinc filter.
+h = np.sinc(2 * fc * (n - (N - 1) / 2))
+ 
+# Compute Blackman window.
+w = 0.42 - 0.5 * np.cos(2 * np.pi * n / (N - 1)) + \
+    0.08 * np.cos(4 * np.pi * n / (N - 1))
+ 
+# Multiply sinc filter by window.
+h = h * w
+ 
+# Normalize to get unity gain.
+h = h / np.sum(h)
+
+
 ###########################################################################
 def get_rms():
     # Creates a generator that can iterate rms values
-    CHUNK = 1024
+    CHUNK = 8
     WIDTH = 2
     CHANNELS = 1
     RATE = 44100
-    colourMax = 255
-
+    
     p = pyaudio.PyAudio()
 
+    
     try:
         stream = p.open(format=p.get_format_from_width(WIDTH),
                         channels=CHANNELS,
                         rate=RATE,
                         input=True,
-                        output=True,
+                        output=False,
                         frames_per_buffer=CHUNK)
-
+        # wait a second to allow the stream to be setup
         time.sleep(1)
         while True:
+            # read the data
             data = stream.read(CHUNK, exception_on_overflow = False)
-            rms = audioop.rms(data, 2)
+            # apply low pass filter
+            rms = audioop.rms(data, 1)
             # Scale the rms value to be within 0-255
             rms_scaled = (rms / 1024) * 255
-            if rms_scaled <= colourMax and rms_scaled >= 0:
+            if rms_scaled <= 255 and rms_scaled >= 0:
                 yield rms_scaled
     finally:
         p.terminate()
